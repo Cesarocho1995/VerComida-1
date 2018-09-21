12ba4a10131282ca9364b3ca424e13da4e916a85
Cesar Mg <cesar_cobalt@hotmail.com>
2018-09-21 00:17:06 -0500

    Initial Commit

---
+++ VerComida/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VerComida
+//
+//  Created by Cesar Enrique Mora Guerra on 9/21/18.
+//  Copyright © 2018 Cesar Enrique Mora Guerra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
